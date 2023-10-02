ab001d52180cf513f6536ef1f886818ffc527c86
ClicianDev <ceedy12345@gmail.com>
2023-10-02 13:17:24 +0800

    update loading and annotation button

--- src/components/canvas/Room.jsx
+++ src/components/canvas/Room.jsx
@@ -16,12 +16,9 @@ function Annotations({ selected, gotoAnnotation }) {
       {annotations.map((a, i) => {
         return (
           <Html key={i} position={[a.lookAt.x, a.lookAt.y, a.lookAt.z]}>
-            <svg height="34" width="34" transform="translate(-16 -16)" style={{ cursor: 'pointer' }}>
-              <circle cx="17" cy="17" r="16" stroke="white" strokeWidth="2" fill="rgba(0,0,0,.66)" onClick={() => gotoAnnotation(i)} />
-              <text x="12" y="22" fill="white" fontSize={17} fontFamily="monospace" style={{ pointerEvents: 'none' }}>
-                {i + 1}
-              </text>
-            </svg>
+            
+              <div className="bloom" onClick={() => gotoAnnotation(i)} />
+       
             {a.description && i === selected && (
               <div id={'desc_' + i} className="annotationDescription text-white-100" dangerouslySetInnerHTML={{ __html: a.description }} />
             )}
